19907b40d9366f2ceb8e539bfa1b164a6561c96f
srighankittu <107845663+srighankittu@users.noreply.github.com>
2025-06-15 21:49:55 +0530

    Added cors and cors options

--- src/app.js
+++ src/app.js
@@ -5,11 +5,18 @@ const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const requestsRouter = require("./routes/requests");
 const { User } = require("./models/user");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
+const CORS_OPTIONS = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true,
+};
 // This middleware converts JSON to js objects
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(CORS_OPTIONS));
 
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
